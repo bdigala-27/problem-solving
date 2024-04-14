ceb68a831e0c57473103790fdb7b4c4139c62fa5
bdigala <bdigala27@gmail.com>
2024-04-14 10:10:14 +0530

    added 55th leetcode question

--- Dynamic Programming/Factorial_number.py
+++ Dynamic Programming/Factorial_number.py
@@ -1,20 +1,20 @@
 import time
 
-# def factorial(n):
-#     dp = [0] * (n + 1)
-#     dp[0] = 1
-#     for i in range(1, n + 1):
-#         dp[i] = dp[i - 1] * i
-#     return dp[n]
+def factorial(n): #memoization
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for i in range(1, n + 1):
+        dp[i] = dp[i - 1] * i
+    return dp[n]
 
-def factorial(n):
+def factorial2(n):
     if n==0 or n==1:
         return 1
     else:
         return (n*factorial(n-1))
 
 start_time = time.time() * 1000  # Start time in milliseconds
-n = 100
+n = 2000
 print(factorial(n))
 end_time = time.time() * 1000  # End time in milliseconds
 

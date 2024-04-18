e7b5990c4bc5e45738ed882b388080e80faaefa7
bdigala <bdigala27@gmail.com>
2024-04-18 22:27:13 +0530

    added inorder traversal code

---
+++ Trees/Inorder_traversal.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,key):
+        self.right = None
+        self.left = None
+        self.data = key
+        
+def inordertraversal(root):
+    st = []
+    curr = root
+    
+    while True:
+        if curr is not None:
+            st.append(curr)
+            curr = curr.left
+        elif(st):
+            curr  = st.pop()
+            print(curr.data, end = " ")
+            curr = curr.right
+        else:
+            break
+    print()
+    
+if __name__ == "__main__":
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+     
+    inordertraversal(root)
+        
